385f4b25001c4dbc3702e9ec3a27713e8b8b8893
Erin Zobitz <erinzobitz@gmail.com>
2019-01-19 15:50:22 -0600

    Make artist preview images go down the page

--- assets/image-preview.js
+++ assets/image-preview.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
   $(".collection-list__link--with-image").hover(function() {
+    var imageHeightDifference = 184;
+    var imageTop = $(this).offset().top - 350;
+
     if (window.innerWidth > 749 && window.innerWidth < 1024) {
-      $(this).parent().next().css('display', 'block');
+      $(this).parent().next().css({ 'display': 'block', 'top': imageTop - imageHeightDifference });
     }
 
     if (window.innerWidth > 1023) {
-      $(this).parent().next().css('display', 'block');
-      $(this).parent().next().next().css('display', 'block');
+      $(this).parent().next().css({ 'display': 'block', 'top': imageTop - imageHeightDifference });
+      $(this).parent().next().next().css({ 'display': 'block', 'top': imageTop });
     }
   });
 
